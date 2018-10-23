6933e4018698081d20079712c9c44e4b6caa20a4
gpwclark <gpwclark@gmail.com>
2018-10-23 03:36:42 +0000

    properly read success from socket for single message

--- src/pricepub/publish.clj
+++ src/pricepub/publish.clj
@@ -24,10 +24,43 @@
            (.clear buf))
          (recur (rest messages)))))))
 
-(defn send-all
+(defn send-messages
   [con-info messages]
     (write-to con-info messages))
 
-(defn send-messages
-  [con-info messages]
-  (send-all con-info messages))
+(defn verify-response
+  [res]
+  (= "OK" (:status (clojure.walk/keywordize-keys (json/read-str res)))))
+
+(defn write-to-once
+  [con-info message]
+  (let
+    [socket-addr (InetSocketAddress. (:host con-info) (:port con-info))]
+    (with-open [socket-chan (SocketChannel/open socket-addr)]
+      (let
+          [write-buf (ByteBuffer/wrap (.getBytes message "UTF-8"))
+            read-buf (ByteBuffer/allocate 1024)]
+        (loop [write-buf write-buf]
+          (when (.hasRemaining write-buf)
+            (.write socket-chan write-buf)
+            (Thread/sleep 10)
+            (recur write-buf)))
+        (println (str "sent: " message))
+        (.clear write-buf)
+        (loop [read-buf read-buf bytes-read 0]
+          (if (<= bytes-read 0)
+            (recur read-buf (.read socket-chan read-buf))
+            (let [position (.position read-buf)
+                  dst-array (byte-array position)
+                  flip (.flip read-buf)
+                  res-bytes (.get read-buf dst-array 0 position)]
+              (String. dst-array))))
+        (.clear read-buf)))))
+
+(defn send-message
+  [con-info message]
+  (let [res (write-to-once con-info message)
+        success (verify-response res)]
+    (if success
+      (println "send message succeeded.")
+      (println "send message failed."))))
